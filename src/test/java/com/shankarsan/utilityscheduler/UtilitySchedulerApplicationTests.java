a094bffb82f69b2d33d359651c5413e1f2ce46c2
Shankarsan Ganai <shankershawn@gmail.com>
2023-07-06 11:03:22 +0530

    Added assert in dummy test

--- src/test/java/com/shankarsan/utilityscheduler/UtilitySchedulerApplicationTests.java
+++ src/test/java/com/shankarsan/utilityscheduler/UtilitySchedulerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.shankarsan.utilityscheduler;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class UtilitySchedulerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
