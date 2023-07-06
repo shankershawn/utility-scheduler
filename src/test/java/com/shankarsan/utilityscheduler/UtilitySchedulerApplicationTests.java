fadc034c52a42f9324723445fc5da15bfd2288be
Shankarsan Ganai <shankershawn@gmail.com>
2023-07-06 11:07:37 +0530

    Used mockito extension in dummy test

--- src/test/java/com/shankarsan/utilityscheduler/UtilitySchedulerApplicationTests.java
+++ src/test/java/com/shankarsan/utilityscheduler/UtilitySchedulerApplicationTests.java
@@ -1,15 +1,17 @@
 package com.shankarsan.utilityscheduler;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
 class UtilitySchedulerApplicationTests {
 
-	@Test
-	void contextLoads() {
-		assertTrue(true);
-	}
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
 
 }
