a24f8dbd8db4359a26411c656b9fc69fe4761622
Shankarsan Ganai <shankershawn@gmail.com>
2023-12-17 21:51:36 +0530

    Revert "Added RefreshScope for ThreadPoolExecutor."
    This reverts commit acbe0a3530c4d7fdcc4cb44ab7b9f84c5c8e9855.

--- src/main/java/com/shankarsan/utilityscheduler/configuration/ThreadPoolConfiguration.java
+++ src/main/java/com/shankarsan/utilityscheduler/configuration/ThreadPoolConfiguration.java
@@ -1,8 +1,6 @@
 package com.shankarsan.utilityscheduler.configuration;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,15 +10,12 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 @RequiredArgsConstructor
-@Slf4j
 public class ThreadPoolConfiguration {
 
     private final ApplicationConfiguration applicationConfiguration;
 
     @Bean
-    @RefreshScope
     public ThreadPoolExecutor getThreadPoolExecutor() {
-        log.debug("Creating getThreadPoolExecutor bean");
         return new ThreadPoolExecutor(applicationConfiguration.getCorePoolSize(),
                 applicationConfiguration.getMaximumPoolSize(), applicationConfiguration.getKeepAliveTimeMillis(),
                 TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
