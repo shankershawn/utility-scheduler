8a9bdfcadc57555c9c30c74d60362032952cd5a4
Shankarsan Ganai <shankershawn@gmail.com>
2024-06-23 16:33:51 +0530

    renamed package

--- src/test/java/com/shankarsan/utilityscheduler/service/comms/impl/HtmlMailServiceImplTest.java
+++ src/test/java/com/shankarsan/utilityscheduler/service/comms/impl/HtmlMailServiceImplTest.java
@@ -1,33 +1,35 @@
-package com.shankarsan.utilityscheduler.service.comms;
+package com.shankarsan.utilityscheduler.service.comms.impl;
 
 import com.shankarsan.utilityscheduler.configuration.ApplicationConfiguration;
 import com.shankarsan.utilityscheduler.dto.EmailDto;
-import com.shankarsan.utilityscheduler.service.comms.impl.SimpleMailServiceImpl;
+import com.shankarsan.utilityscheduler.exception.ApplicationException;
+import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.io.File;
 import java.util.List;
+import java.util.Properties;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class SimpleMailServiceImplTest {
+public class HtmlMailServiceImplTest {
 
     @Mock
-    private MailSender javaMailSender;
+    private JavaMailSenderImpl javaMailSender;
 
     @Mock
     private ApplicationConfiguration applicationConfiguration;
 
     @InjectMocks
-    private SimpleMailServiceImpl htmlMailService;
+    private HtmlMailServiceImpl htmlMailService;
 
     private static List<EmailDto> recipients;
 
@@ -43,9 +45,7 @@ public class SimpleMailServiceImplTest {
                 EmailDto.builder().emailAddress("abc@def.com")
                         .emailAddressLevel(EmailDto.EmailAddressLevel.TO).build(),
                 EmailDto.builder().emailAddress("mno@pqr.com")
-                        .emailAddressLevel(EmailDto.EmailAddressLevel.BCC).build(),
-                EmailDto.builder().emailAddress("rst@uvw.com")
-                        .emailAddressLevel(EmailDto.EmailAddressLevel.CC).build()
+                        .emailAddressLevel(EmailDto.EmailAddressLevel.BCC).build()
         );
         body = "Test body";
         subject = "Test subject";
@@ -56,15 +56,22 @@ public class SimpleMailServiceImplTest {
     void shouldReturnWhenMailFlagIsFalse() {
         lenient().when(applicationConfiguration.getMailFlag()).thenReturn(Boolean.FALSE);
         htmlMailService.sendMail(recipients, body, subject, attachments);
-        verify(javaMailSender, times(0)).send(any(SimpleMailMessage.class));
+        verify(javaMailSender, times(0)).send(any(MimeMessage.class));
     }
 
     @Test
     void shouldSendMail() {
         when(applicationConfiguration.getMailFlag()).thenReturn(Boolean.TRUE);
+        when(javaMailSender.getJavaMailProperties()).thenReturn(new Properties());
         htmlMailService.sendMail(recipients, body, subject, attachments);
-        // Second time call is for coverage
-        htmlMailService.sendMail(recipients, body, subject, attachments);
-        verify(javaMailSender, times(2)).send(any(SimpleMailMessage.class));
+        verify(javaMailSender, times(1)).send(any(MimeMessage.class));
+    }
+
+    @Test
+    void shouldThrowApplicationException() {
+        when(applicationConfiguration.getMailFlag()).thenThrow(ApplicationException.class);
+        assertThrows(ApplicationException.class,
+                () -> htmlMailService.sendMail(recipients, body, subject, attachments));
+        verify(javaMailSender, times(0)).send(any(MimeMessage.class));
     }
 }
