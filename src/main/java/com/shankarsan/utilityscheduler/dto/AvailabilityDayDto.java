ad14f1bb79b2cdd328ccbfe7630fadb125361ea2
Shankarsan Ganai <shankershawn@gmail.com>
2024-12-08 14:11:40 +0530

    handled null pointer when AvailabilityDayDto is not found in cache

--- src/main/java/com/shankarsan/utilityscheduler/dto/AvailabilityDayDto.java
+++ src/main/java/com/shankarsan/utilityscheduler/dto/AvailabilityDayDto.java
@@ -15,58 +15,60 @@ import java.util.Optional;
 @Slf4j
 public class AvailabilityDayDto implements Serializable, Comparable<AvailabilityDayDto> {
 
-    private static final long serialVersionUID = -3L;
+  private static final long serialVersionUID = -3L;
 
-    @JsonProperty("availablityDate")
-    private String availabilityDate; //18-7-2023
-    @JsonProperty("availablityStatus")
-    private String availabilityStatus; //RLWL1/WL1
-    private Integer availabilityStatusRank;
-    private transient String reasonType; //S
-    private transient Integer availabilityChange;
-    @JsonProperty("availablityType")
-    private transient String availabilityType; //3
-    private transient String currentBkgFlag; //N
-    private transient String wlType; //12
+  @JsonProperty("availablityDate")
+  private String availabilityDate; //18-7-2023
+  @JsonProperty("availablityStatus")
+  private String availabilityStatus; //RLWL1/WL1
+  private Integer availabilityStatusRank;
+  private transient String reasonType; //S
+  private transient Integer availabilityChange;
+  @JsonProperty("availablityType")
+  private transient String availabilityType; //3
+  private transient String currentBkgFlag; //N
+  private transient String wlType; //12
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    AvailabilityDayDto that = (AvailabilityDayDto) o;
+    return this.availabilityDate.equals(that.availabilityDate)
+        && this.availabilityStatus.equals(that.availabilityStatus);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(availabilityDate, availabilityStatus);
+  }
+
+  public void setAvailabilityChange(AvailabilityDayDto o) {
+    log.debug("this = {}, o = {}", this, o);
+    Optional.ofNullable(o)
+        .map(this::compareTo)
+        .ifPresent(change -> this.availabilityChange = change);
+  }
+
+  @Override
+  public int compareTo(@NonNull AvailabilityDayDto o) {
+
+    if (Optional.ofNullable(this.availabilityStatusRank).isPresent()
+        && Optional.ofNullable(o.availabilityStatusRank).isPresent()) {
+      if (Objects.equals(this.availabilityStatusRank, o.availabilityStatusRank)) {
+        if ("1".equals(this.availabilityType)) {
+          return this.availabilityStatus.compareTo(o.availabilityStatus);
+        } else {
+          return o.availabilityStatus.compareTo(this.availabilityStatus);
         }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        AvailabilityDayDto that = (AvailabilityDayDto) o;
-        return this.availabilityDate.equals(that.availabilityDate)
-                && this.availabilityStatus.equals(that.availabilityStatus);
+      } else {
+        return o.availabilityStatusRank - this.availabilityStatusRank;
+      }
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(availabilityDate, availabilityStatus);
-    }
-
-    public void setAvailabilityChange(AvailabilityDayDto o) {
-        log.debug("this = {}, o = {}", this, o);
-        this.availabilityChange = compareTo(o);
-    }
-
-    @Override
-    public int compareTo(@NonNull AvailabilityDayDto o) {
-
-        if (Optional.ofNullable(this.availabilityStatusRank).isPresent()
-                && Optional.ofNullable(o.availabilityStatusRank).isPresent()) {
-            if (Objects.equals(this.availabilityStatusRank, o.availabilityStatusRank)) {
-                if ("1".equals(this.availabilityType)) {
-                    return this.availabilityStatus.compareTo(o.availabilityStatus);
-                } else {
-                    return o.availabilityStatus.compareTo(this.availabilityStatus);
-                }
-            } else {
-                return o.availabilityStatusRank - this.availabilityStatusRank;
-            }
-        }
-        return 0;
-    }
+    return 0;
+  }
 }
